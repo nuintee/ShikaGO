de1cd924c42f5815b2f03daee72640dc3ecfe46e
staff <staff@iMacA.local>
2020-11-13 01:45:56 +0900

    post modal base done

--- html/js/modals.js
+++ html/js/modals.js
@@ -1,20 +1,21 @@
 
 let posts = document.querySelectorAll('.m-content-panel.post');
-let modal_post = document.querySelector('.m-modal.post');
+let modal_post = document.querySelector('.m-modal-bg.post');
+let modal_member = document.querySelector('.m-modal-bg.member');
 let modal_bg = document.querySelector('.m-modal-bg');
 let member = document.querySelectorAll('.m-member');
 
 for (let i = 0; i < member.length; i++) {
     member[i].addEventListener('click',function(e){
         console.log(e.target);
-        modal_bg.classList.add('show');
+        modal_member.classList.add('show');
         if (e.target.className == "m-member is-online"){
-            modal_bg.children[0].querySelector('img').style.border = "greenyellow solid 0.3em";
-            modal_bg.children[0].querySelector('.modal-top').querySelector('h4').style.color = "#FFF";
+            modal_member.children[0].querySelector('img').style.border = "greenyellow solid 0.3em";
+            modal_member.children[0].querySelector('.modal-top').querySelector('h4').style.color = "#FFF";
         }
         else{
-            modal_bg.children[0].querySelector('img').style.border = "none";
-            modal_bg.children[0].querySelector('.modal-top').querySelector('h4').style.color = "#8D8D8D";
+            modal_member.children[0].querySelector('img').style.border = "none";
+            modal_member.children[0].querySelector('.modal-top').querySelector('h4').style.color = "#8D8D8D";
         }
     },false);
 }
@@ -23,10 +24,10 @@ modal_bg.addEventListener('click',function(e){
     e.target.classList.remove('show');
 });
 
-/* post_modal
+/* post_modal */
 for (let i = 0; i < posts.length; i++) {
     posts[i].addEventListener('click',function(e){
-        modal.classList.add('show');
+        modal_post.classList.add('show');
         let parent = e.target.parentNode;
         let grand_p = parent.parentNode;
         let post_img = parent.querySelector('img');
@@ -35,10 +36,15 @@ for (let i = 0; i < posts.length; i++) {
         let post_date = "date : "+parent.querySelector('.post-footer').querySelector('.post-date').innerHTML;
         let post_author = "author: "+parent.querySelector('.post-footer').querySelector('.post-author').innerHTML;
         //modal elements
-        modal.querySelector('.modal-img.post').src = post_img.src;
+        let modal_content = modal_post.querySelector('.m-content-panel.project.dis');
+        modal_content.querySelector('img').src = post_img.src;
+        console.log()
     },false);
 }
-*/
+
+modal_post.addEventListener('click',function(e){
+    e.target.classList.remove('show');
+});
 
 function hide_modal(x){
     x.parentNode.classList.remove('show');

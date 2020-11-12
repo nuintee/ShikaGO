d3d9d33d959dde0114e8830c4983f19b6f03c166
staff <staff@iMacA.local>
2020-11-12 13:27:23 +0900

    all pages exept admin and modals

--- html/index.php
+++ html/index.php
@@ -116,6 +116,44 @@
                     </div>
                 </div>
            </div>
+           <div class = "m-bot-page">
+                <?php echo '<p class = "m-page-title">公式bot <span class = "m-under-constuction dis txt">[開発中]</span></p>' ?>
+                <div class = "m-content-panel project dis">
+                    <img src="./images/robot.jpg" alt="">
+                         <div class = "content project">   
+                         <h4 class = "post-title">ShicaGOとは</h4>
+                         <p class = "content-body">
+                         カタカナ語が苦手な方は「組見本」と呼ぶとよいでしょう。本文用なので使い方を間違えると不自然に見えることもありますので要注意。これは正式な文章の代わりに入れて使うダミーテキストです。主に書籍やウェブページなどのデザインを作成する時によく使われます。文章に特に深い意味はありません。なお、組見本の「組」とは文字組のことです。活字印刷時代の用語だったと思います。
+                         </p>
+                     </div>
+                </div>
+                <?php echo '<p class = "m-page-title">Botコマンド一覧</p>' ?>
+                <div class = "m-content-panel">
+                    <div class = "content">
+                        <h4 class = "post-title">!help</h4>
+                        <p class = "content-body">
+                            Botコマンドの表示
+                        </p>
+                        <h4 class = "post-title">!bmi 身長体重</h4>
+                        <p class = "content-body">
+                            BMI指数の表示
+                        </p>
+                        <h4 class = "post-title">!decide 選択肢A 選択肢B 選択肢C (何個でも可能)</h4>
+                        <p class = "content-body">
+                            選択肢からランダムで一つを選択
+                        </p>
+                        <h4 class = "post-title">!movie タイトル</h4>
+                        <p class = "content-body">
+                        該当映画のポスターと平均ユーザースコアを表示<br>*TMDB API使用
+                        </p>
+                        <h4 class = "post-title">!rate タイトル 評価点(1 - 10)</h4>
+                        <p class = "content-body">
+                        該当映画の評価を行う (1 - 10点)<br>要TMDBアカウント<br>*TMDB API使用
+                        </p>
+                        <input type="submit" class = "m-submit-btn" value = "サーバーに招待する">
+                    </div>
+                </div>
+           </div>
            <div class ="admin">
            <?php echo '<p class = "m-page-title">管理者ログイン</p>' ?>
                 <div class = "m-content-panel">
@@ -124,7 +162,7 @@
                             <h4 class = "post-title">ユーザー名</h4>
                             <input type="text" class = "m-input single-line" placeholder = "ユーザー名">
                             <h4 class = "post-title">パスワード</h4>
-                            <input type="password" class = "m-input single-line" placeholder = "ユーザー名">
+                            <input type="password" class = "m-input single-line" placeholder = "パスワード">
                             <input type="submit" class = "m-submit-btn" value = "ログイン">
                         </form>
                     </div>

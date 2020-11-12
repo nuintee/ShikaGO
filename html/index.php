5ef66a0e826779bafff19f7b2279a62c2433fc4d
staff <staff@iMacA.local>
2020-11-12 19:33:47 +0900

    modal post base done

--- html/index.php
+++ html/index.php
@@ -18,10 +18,21 @@
 <body>
     <div class = "l-main">
             <div class = "m-modal post">
+                <button class = "m-close-modal-btn" onclick = "hide_modal(this)">
+                    <i class="fas fa-times"></i>
+                </button>
                 <div class = "m-modal-panel">
-                    <button class = "m-close-modal-btn" onclick = "hide_modal(this)">
-                        
-                    </button>
+                    <img src="./images/robot.jpg" alt="modal_image" class = "modal-img post">
+                    <div class = "content post">
+                        <h4 class = "post-title">投稿開始!</h4>
+                        <p class = "content-body post">
+                        カタカナ語が苦手な方は「組見本」と呼ぶとよいでしょう。本文用なので使い方を間違えると不自然に見えることもありますので要注意。これは正式な文章の代わりに入れて使うダミーテキストです。主に書籍やウェブページなどのデザインを作成する時によく使われます。文章に特に深い意味はありません。なお、組見本の「組」とは文字組のことです。活字印刷時代の用語だったと思います。
+                        </p>
+                        <div class = "post-footer">
+                            <p class = "post-date">2020/11/18/22:30</p>
+                            <p class = "post-author">投稿者 : イッシー</p>
+                        </div>
+                    </div>
                 </div>
            </div>
        <aside class = "l-left-bar">
--- html/js/modals.js
+++ html/js/modals.js
@@ -7,14 +7,16 @@ for (let i = 0; i < posts.length; i++) {
         modal.classList.add('show');
         let parent = e.target.parentNode;
         let grand_p = parent.parentNode;
+        let post_img = parent.querySelector('img');
         let post_title = "title : "+parent.querySelector('.post-title').innerHTML;
         let post_description = "description : "+parent.querySelector('.content-body.post').innerHTML;
         let post_date = "date : "+parent.querySelector('.post-footer').querySelector('.post-date').innerHTML;
         let post_author = "author: "+parent.querySelector('.post-footer').querySelector('.post-author').innerHTML;
+        //modal elements
+        modal.querySelector('.modal-img.post').src = post_img.src;
     },false);
 }
 
 function hide_modal(x){
-    let modal_bg = x.parentNode.parentNode;
-    modal_bg.classList.remove('show');
+    x.parentNode.classList.remove('show');
 }

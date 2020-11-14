9f8819b213ea0a43fce6f9d91deade8b1b5e1450
staff <staff@iMacA.local>
2020-11-14 20:10:27 +0900

    updated index reference directory

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php include_once './includes/conn.inc.php' ?>
-<?php include_once './php/header.php'?>
-<?php include_once "./modals.html" ?>
+<?php include_once './html/includes/conn.inc.php' ?>
+<?php include_once './html/php/header.php'?>
+<?php include_once "./html/modals.html" ?>
 <div class = "m-post-panels is-active" data-index = "1">
     <p class = "m-page-title">投稿一覧</p>
     <?php 

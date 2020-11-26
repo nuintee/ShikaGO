bbc1d3115c674ce661e03b44de72becf6f7697c0
staff <staff@iMacA.local>
2020-11-26 19:27:03 +0900

    hiding status bar

--- html/php/header.php
+++ html/php/header.php
@@ -13,6 +13,8 @@
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+            <meta name="apple-mobile-web-app-capable" content="yes">
+            <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
             <meta http-equiv="Cache-Control" content="no-cache">
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
             <link rel="stylesheet" href="./css/base.css">
